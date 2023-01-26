d338378c33d2dd2c12197ffb335bf0b18205c2f9
Oscarachieng <oscarachieng94@gmail.com>
2023-01-26 10:17:51 +0300

    Adds CreateAccount and Signin template and their stylings [client]

--- src/coomponents/accounts/CreateAcccount.js
+++ src/coomponents/accounts/CreateAcccount.js
@@ -1,79 +1,104 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const CreateAcccount = () => {
   // state variable to hold the registration data
-  const [registrationData, setRegistrationData] = useState({ firstName: '', lastName: '', email: "", cellPhoneNumber: "", residence: "", cluster: "" })
-  
-  // handling onChange event 
+  const [registrationData, setRegistrationData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    cellPhoneNumber: "",
+    
+    cluster: "",
+    password: "",
+    confrimPassword: ""
+  });
+
+  // handling onChange event
   const handleChange = (event) => {
     const name = event.target.name;
     const value = event.target.value;
     setRegistrationData({ ...registrationData, [name]: value });
-  }
+  };
 
   return (
-    <div> Create Account :
-      <form>
-        <h5>First Name:</h5>
-        <input
-          type="text"
-          name="firstName"
-          onChange={handleChange}
-          value={registrationData.firstName}
-          placeholder="Enter your First Name here"
-          required
-        />
+    <div>
+      <h4 className="form-header">Create Account :</h4>
+      <div className="container">
+        <form>
+          <h5>First Name:</h5>
+          <input
+            type="text"
+            name="firstName"
+            onChange={handleChange}
+            value={registrationData.firstName}
+            placeholder="Enter your First Name here"
+            required
+          />
 
-        <h5>Last Name:</h5>
-        <input
-          type="text"
-          name="lastName"
-          onChange={handleChange}
-          value={registrationData.lastName}
-          placeholder="Enter your Last Name here"
-          required
-        />
-        <h5>Email:</h5>
-        <input
-          type="text"
-          name="email"
-          onChange={handleChange}
-          value={registrationData.email}
-          placeholder="Enter your Email here"
-          required
-        />
-        <h5>CellPhone Number:</h5>
-        <input
-          type="text"
-          name="cellPhoneNumber"
-          onChange={handleChange}
-          value={registrationData.cellPhoneNumber}
-          placeholder="Enter your mobile Number here"
-          required
-        />
-        <h5>Area of Residense:</h5>
-        <input
-          type="text"
-          name="residense"
-          onChange={handleChange}
-          value={registrationData.residence}
-          placeholder="Enter your residence here"
-          required
-        />
-        <h5>Cluster:</h5>
-        <input
-          type="text"
-          name="cluster"
-          onChange={handleChange}
-          value={registrationData.cluster}
-          placeholder="Enter your Cluster here"
-          required
-        />
+          <h5>Last Name:</h5>
+          <input
+            type="text"
+            name="lastName"
+            onChange={handleChange}
+            value={registrationData.lastName}
+            placeholder="Enter your Last Name here"
+            required
+          />
+          <h5>Email:</h5>
+          <input
+            type="text"
+            name="email"
+            onChange={handleChange}
+            value={registrationData.email}
+            placeholder="Enter your Email here"
+            required
+          />
+          <h5>CellPhone Number:</h5>
+          <input
+            type="text"
+            name="cellPhoneNumber"
+            onChange={handleChange}
+            value={registrationData.cellPhoneNumber}
+            placeholder="Enter your mobile Number here"
+            required
+          />
+          
+          <h5>Cluster:</h5>
+          <input
+            type="text"
+            name="cluster"
+            onChange={handleChange}
+            value={registrationData.cluster}
+            placeholder="Enter your Cluster here"
+            required
+          />
+          <h5>Enter Password:</h5>
+          <input
+            type="text"
+            name="password"
+            onChange={handleChange}
+            value={registrationData.password}
+            placeholder="Enter your password here"
+            required
+          />
+          <h5>Confirm Password:</h5>
+          <input
+            type="text"
+            name="confirmPassword"
+            onChange={handleChange}
+            value={registrationData.confrimPassword}
+            placeholder="Enter your password here"
+            required
+          />
+          <br />
 
-        <button type="submit">Register</button>
-      </form>
+          <button className="submit-button" type="submit">
+            Register
+          </button>
+        </form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default CreateAcccount
+export default CreateAcccount;
--- src/coomponents/accounts/SignIn.js
+++ src/coomponents/accounts/SignIn.js
@@ -1,8 +1,48 @@
-import React from 'react'
+import React , { useState } from 'react';
+
 
 const SignIn = () => {
+  // State variable
+  const [userDate, setUserData] = useState({  email: "", password: "" })
+  
+  // handling onChange event 
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setUserData({ ...userDate, [name]: value });
+  }
+
   return (
-    <div>SignIn Form is Here</div>
+    <div >
+      <h4 className='form-header'>Login Here.</h4>
+       <div className='container'> 
+      <form>
+        
+        <h5>Email:</h5>
+        <input
+          type="text"
+          name="email"
+          onChange={handleChange}
+          value={userDate.email}
+          placeholder="Enter your Email here"
+          required
+        />
+        <h5>password:</h5>
+        <input
+          type="text"
+          name="cellPhoneNumber"
+          onChange={handleChange}
+          value={userDate.password}
+          placeholder="Enter your mobile Number here"
+          required
+        />
+        <br/>
+
+        <button className='submit-button' type="submit">Login</button>
+      </form>
+    </div>
+
+</div>
   )
 }
 
